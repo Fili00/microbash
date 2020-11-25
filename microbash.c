6a2c1c145dfe89bb24e8d449e6911aaee6ae7a1e
Fili00 <fsiri2000@gmail.com>
2020-11-25 22:33:01 +0100

    cambiato path

--- microbash.c
+++ microbash.c
@@ -3,45 +3,33 @@
 #include <stdlib.h>
 #include <string.h>
 
-char* currentDir(const char* path){
-    char* buf;
-    if((buf = calloc(strlen(path)+1, sizeof(char))) == NULL)
-        perror("calloc error: ");
-
-    strcpy(buf, path);
-
-    char *dir = NULL;
+void currentDir(char* dir, long size){
+    char* path;
     char *saveptr = NULL;
 
-    for (buf = strtok_r(buf, "/", &saveptr); buf != NULL; buf = strtok_r(NULL, "/", &saveptr))
-        dir = buf;
+    if((path = (char *)malloc((size_t)size)) == NULL)
+        perror("malloc error: ");
 
-    return dir;
+    if (getcwd(path, (size_t) size) == 0)
+        perror("getcwd error: ");
+
+    strcpy(dir,"/");
+
+    for (path = strtok_r(path, "/", &saveptr); path != NULL; path = strtok_r(NULL, "/", &saveptr))
+        strcpy(dir,path);
+
+    free(path);
 }
 
-void cd(char* path, char* np){
-    char* buf;
-    if((buf = calloc(strlen(path)+1, sizeof(char))) == NULL)
-        perror("calloc error: ");
-    strcpy(buf, path);
-
-    if(*np == '/')
-        strcpy(path, np);
-    else if(!strncmp(np,"..",2)){
-        ;
-    }else{
-        strcat(path,"/");
-        strcat(path,np);
+int cd(char* np){
+    int res;
+    if((res = chdir(np))) {
+        printf("No such file or directory: %s\n", np);
     }
-
-    if(chdir(np)) {
-        printf("[%s]",np);
-        strcpy(path, buf);
-    }
-    free(buf);
+    return res;
 }
 
-void parser(char* path, char* cmd){
+void parser(char* cmd){
     cmd[strlen(cmd)-1]=0; //tolgo \n alla fine della riga
 
     char* buf;
@@ -50,7 +38,6 @@ void parser(char* path, char* cmd){
 
     strcpy(buf, cmd);
 
-    char *dir = NULL;
     char *saveptr = NULL;
     char* saveptrarg = NULL;
 
@@ -66,32 +53,30 @@ void parser(char* path, char* cmd){
         char ** argv = malloc(sizeof(char*)*(countspace+1));
         i=0;
         for (arg = strtok_r(buf, " ", &saveptrarg); arg != NULL; arg = strtok_r(NULL, " ", &saveptrarg),i++) {
-            printf("Arg:%s\n", arg);
             argv[i]=malloc(strlen(arg));
             strcpy(argv[i],arg);
         }
 
         if(!strcmp(argv[0],"cd"))
-            cd(path, argv[1]);
+            cd( argv[1]);
         else
             ; //roba varia noiosa
-
     }
 }
 
 int main() {
-    char* path;
+    char* dir;
     long size = pathconf(".", _PC_PATH_MAX);
-    if((path = (char *)malloc((size_t)size)) == NULL)
+    if((dir = (char *)malloc((size_t)size)) == NULL)
         perror("malloc error: ");
-    if (getcwd(path, (size_t) size) == 0)
-        perror("getcwd error: ");
 
     char cmd[500];
 
     for(;;) {
-        printf("%s $ ", currentDir(path));
+        currentDir(dir, size);
+        printf("%s $ ", dir);
         fgets(cmd, 500, stdin);
-        parser(path, cmd);
+        parser(cmd);
     }
+    free(dir);
 }
