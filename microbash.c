d2b548c4b24d888f1fff1ffa94e357cb991bffc0
Fili00 <fsiri2000@gmail.com>
2020-12-01 23:24:36 +0100

    quasi finale

--- microbash.c
+++ microbash.c
@@ -8,12 +8,26 @@
 #include <ctype.h>
 #include <errno.h>
 
+void clean(char ** v){
+    int i=0;
+    while(v[i]!=NULL)
+        free(v[i++]);
+    free(v);
+    v=NULL;
+}
+
 void currentDir(char* dir, long size){
     char* path;
     char *saveptr = NULL;
+    
 
-    if((path = (char *)malloc((size_t)size)) == NULL)
+    if((path = ((char *)malloc(((size_t)size)+1))) == NULL) {
         perror("malloc error: ");
+        exit(EXIT_FAILURE);
+    }
+
+    char * tmp = path;
+
     //path contiene il percorso assoluto attuale
     if (getcwd(path, (size_t) size) == 0)   
         perror("getcwd error: ");
@@ -23,7 +37,7 @@ void currentDir(char* dir, long size){
     for (path = strtok_r(path, "/", &saveptr); path != NULL; path = strtok_r(NULL, "/", &saveptr))
         strcpy(dir,path);
 
-    free(path);
+    free(tmp);
 }
 
 int cd(char** argv){
@@ -34,38 +48,37 @@ int cd(char** argv){
     int res;
     if((res = chdir(argv[1])) != 0)
         printf("No such file or directory: %s\n", argv[1]);
+    clean(argv);
     return res;
 }
 
-void clean(char ** v){
-    int i=0;
-    while(v[i]!=NULL)
-        free(v[i++]);
-    free(v);
-    v=NULL;
-}
+
 
 void my_exec(char** argv, char **envp, int fdIn, int fdOut){
     pid_t pid = fork();
     if(pid<0){
         perror("Fork failed: ");
-        printf("Insert new command\n");
-        return;
+        exit(EXIT_FAILURE);
     }
     if(pid == 0){
         if(fdOut!=STDOUT_FILENO) {
-            dup2(fdOut, STDOUT_FILENO);
+            if(dup2(fdOut, STDOUT_FILENO) < 0){
+                perror("dup");
+                exit(EXIT_FAILURE);
+            }
             close(fdOut);
         }
         if(fdIn!=STDIN_FILENO) {
-            dup2(fdIn, STDIN_FILENO);
+            if(dup2(fdIn, STDIN_FILENO) < 0){
+                perror("dup");
+                exit(EXIT_FAILURE);
+            }
             close(fdIn);
         }
 
-        execvp(argv[0],argv); //variabili d'ambiente
+        execvp(argv[0],argv);
         perror("Error executing the command");
         clean(argv);
-        clean(envp);
         exit(EXIT_FAILURE);
     }
     if(fdOut!=STDOUT_FILENO)
@@ -73,82 +86,66 @@ void my_exec(char** argv, char **envp, int fdIn, int fdOut){
     if(fdIn!=STDIN_FILENO)
         close(fdIn);
     clean(argv);
-    clean(envp);
 }
 
-void parserArg(char* cmd, char*** argv, char*** envp, int* fdIn, int* fdOut){
+int parserArg(char* cmd, char*** argv, char*** envp, int* fdIn, int* fdOut){
     if(cmd[strlen(cmd)-1]=='\n')
         cmd[strlen(cmd)-1]='\0';
-    int i=0;
-    int k=0;
+    int i;
     int argCount=2;
-    int envCount=1;
     char* arg;
     char* saveptr;
 
-    int check = 0;
-
     for(i=0; cmd[i]!='\0';i++) {
-        if (cmd[i] == ' ') {
+        if (cmd[i] == ' ')
             argCount++;
-            check = 1;
-        }
-        if (cmd[i] == '$'){
-            if(check){
-                argCount--;
-                envCount++;
-                check = 1;
-            }
-        }
+        else if (cmd[i] == '<' || cmd[i] == '>')
+            argCount--;
     }
     *argv = malloc(sizeof(char*)*(argCount));
-    *envp = malloc(sizeof(char*)*(envCount));
-
+    if(*argv == NULL){
+        perror("malloc");
+        exit(EXIT_FAILURE);
+    }
 
     *fdOut = STDOUT_FILENO;
     *fdIn = STDIN_FILENO;
-
-    check = 0;
 
     for (arg = strtok_r(cmd, " ", &saveptr), i=0; arg != NULL; arg = strtok_r(NULL, " ", &saveptr)) {
         if(!strncmp(arg,"$",1)){
             char * name = arg+1;
             char * value = getenv(name);
-            if(value==NULL){
-                value=malloc(sizeof(char));
-                strcpy(value,"");
+            if(value==NULL)
+                continue;
+            (*argv)[i]=malloc(strlen(value)+1);
+            if((*argv)[i] == NULL){
+                perror("malloc");
+                exit(EXIT_FAILURE);
             }
-
-            if(!check){
-                if(!strcmp(value,""))
-                    continue;
-                (*argv)[i]=malloc(strlen(value));
-                strcpy((*argv)[i],value);
-                i++;
-                check = 1;
-            }else{
-                (*envp)[k]=malloc(strlen(name)+strlen(value));
-                strcpy((*envp)[k],name);
-                strcat((*envp)[k],"=");
-                strcat((*envp)[k],value);
-                k++;
-            }
-
+            strcpy((*argv)[i],value);
+            i++;
         }else if(arg[0]=='>') {
-            if((*fdOut = open(arg + 1, O_WRONLY|O_CREAT, 0644)) < 0)
+            if((*fdOut = open(arg + 1, O_WRONLY|O_CREAT, 0644)) < 0) {
                 perror("errore >");
+                return 0;
+            }
         }else if(arg[0]=='<'){
-            if((*fdIn = open(arg + 1, O_RDONLY)) < 0)
+            if((*fdIn = open(arg + 1, O_RDONLY)) < 0) {
                 perror("errore <");
+                return 0;
+            }
         }else{
-            (*argv)[i]=malloc(strlen(arg));
+            (*argv)[i]=malloc(strlen(arg)+1);
+            if((*argv)[i] == NULL){
+                perror("malloc");
+                exit(EXIT_FAILURE);
+            }
             strcpy((*argv)[i],arg);
             i++;
-            check = 1;
         }
     }
     (*argv)[i]=NULL;
-    (*envp)[k]=NULL;
+    return 1;
 }
 
 void cmdHandler (char *cmd){
@@ -165,7 +162,8 @@ void cmdHandler (char *cmd){
 
     arg = strtok_r(cmd, "|", &saveptr);
     while(arg != NULL) {
-        parserArg(arg, &argv, &envp, &fdIn, &fdOut);
+        if(!parserArg(arg, &argv, &envp, &fdIn, &fdOut))
+            break;
         if (check) {
             fdIn = pipefd[0];
         }
@@ -187,7 +185,10 @@ void cmdHandler (char *cmd){
     }
     for(i=0; i<n_proc; i++) {
         wait(&status);
-        //mettere controllo su status
+        if(WIFEXITED(status)){
+            if(WEXITSTATUS(status))
+                printf("Command failed\n");
+        }
     }
 }
 
@@ -226,24 +227,47 @@ int validate(char* cmd){
     if(cmd[strlen(cmd)-1] == '<' || cmd[strlen(cmd)-1] == '>')
         return 0;
     for(i=0;cmd[i] != '\0'; i++){
-        if(cmd[i] == '|')
+        if(cmd[i] == '|'){
+            if(cmd[i+1] != ' ')
+                return 0;
             checkPipe = 1;
-        if(cmd[i] == '|' && checkRedirect)
-            return 0;
-        if(checkPipe && cmd[i] == '<')
-            return 0;
-        if(cmd[i] == '>')
+            if(checkRedirect)
+                return 0;
+            if(!strncmp(&cmd[i + 2], "cd", 2))
+                return 0;
+            if(cmd[i+2] == '|')
+                return 0;
+            if(cmd[i+2] == '<' || cmd[i+2] == '>')
+                return 0;
+        }else if(cmd[i] == '>'){
             checkRedirect = 1;
-        if(cmd[i] == '|' && (cmd[i+2] == '<' || cmd[i+2] == '>'))
+        }else if(cmd[i] == '<' && checkPipe)
             return 0;
         if((cmd[i] == '|' || cmd[i] == '<' || cmd[i] == '>') && checkCD)
             return 0;
         if((cmd[i] == '>' || cmd[i] == '<') && isspace(cmd[i+1]))
             return 0;
-        if(cmd[i]=='|' && ((isspace(cmd[i+1]) && cmd[i+2] == '|' ) || cmd[i+1]=='|') )
+    }
+/*
+    for(i=0;cmd[i] != '\0'; i++){
+        if(cmd[i] == '|') //messo
+            checkPipe = 1;
+        if(cmd[i] == '|' && checkRedirect) //messo
+            return 0;
+        if(checkPipe && cmd[i] == '<') //messo
+            return 0;
+        if(cmd[i] == '>') //messo
+            checkRedirect = 1;
+        if(cmd[i] == '|' && (cmd[i+2] == '<' || cmd[i+2] == '>')) //messo
+            return 0;
+        if((cmd[i] == '|' || cmd[i] == '<' || cmd[i] == '>') && checkCD) //messo
+            return 0;
+        if((cmd[i] == '>' || cmd[i] == '<') && isspace(cmd[i+1])) //messo
+            return 0;
+        if(cmd[i]=='|' && ((isspace(cmd[i+1]) && cmd[i+2] == '|' ) || cmd[i+1]=='|') ) //messo
             return 0;
 
-        if(cmd[i] == '|'){
+        if(cmd[i] == '|'){ //messo
             if(isspace(cmd[i+1]))
                 i++;
             if (!strncmp(&cmd[i + 1], "cd", 2))
@@ -252,14 +276,17 @@ int validate(char* cmd){
         }
     }
 
+*/
     return 1;
 }
 
 int main() {
     char* dir;
     long size = pathconf(".", _PC_PATH_MAX);
-    if((dir = (char *)malloc((size_t)size)) == NULL)
+    if((dir = (char *)malloc((size_t)size)) == NULL) {
         perror("malloc error: ");
+        exit(EXIT_FAILURE);
+    }
 
     char cmd[1024];
 
@@ -275,6 +302,8 @@ int main() {
         if(!strcmp(cmd,"exit"))
             break;
         cmdHandler(cmd);
+
     }
     free(dir);
+
 }
